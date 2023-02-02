2e6800319c67ac7f396d48f3c5f78a7a30171691
gweryf <priyam2007sp@gmail.com>
2023-02-02 18:59:27 +0530

    added images for homepage and styled the homepage

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,19 +1,102 @@
 import React from "react";
+import service1 from "../components/images/service1.jpg"
+import service2 from "../components/images/service2.jpg"
+import service3 from "../components/images/service3.jpg"
+import service4 from "../components/images/service4.jpg"
+import service5 from "../components/images/service5.png"
+import arrow from "../components/images/arrow.png"
 import LandingPageFooter from '../components/LandingPage/LandingPage_footer'
 
 const LandingPage = () => {
   return (
   <div>
+    <div className=' text-center p-10 py-10'>
+      <h2 className=' text-5xl py-2 text-teal-600 font-medium md:text-5xl text-bold'>reTrade</h2>
+      <h3 className='text-2xl py-2 md:text-2xl'>Exchange More, Spend Less: Join the Revolution on reTrade</h3>
+      <p className=' text-lg py-5 leading-8 text-gray-500 md:text-xl max-w-xl mx-auto'>
+      Whether you want to trade up your old items for something new, or simply make room for the things you love, our community of users offers a smart and sustainable solution. So why wait? Join <span className=" text-teal-600">reTrade</span> and start swapping smarter, not harder!
+      </p>
 
-    <div className="max-h-full p-2 items-center h-screen">
-     <div className='items-center text-center w-full p-2'>
-      <h1 className="text-5xl text-[#4dc3b5]">reTrade!</h1>
-      <h5 >
-         It a way of dealing with something amazing
-      </h5>
-     </div>
+      <div>
+        <h2 className=" text-2xl mt-10">
+          Ready to start trading smarter, not harder?
+        </h2>
+        <div className=" flex mx-auto text-center justify-center">
+          <p className=" text-lg py-5 leading-8 text-gray-500 md:text-xl ">
+            Click below to sign-up right away!
+          </p>
+          <img src={arrow} className="" width={200} height={200}  alt="" />
+        </div>
 
+        <button className=" text-xl ml-10 mb-10 bg-teal-600 py-3 px-4 rounded-lg text-white hover:scale-110 transition duration-300 ease-in-ou">
+          Sign-Up
+        </button>
+        
+      </div>
     </div>
+
+    <section className=" text-white bg-black">
+      <div className=" p-2">
+        <h3 className=" text-2xl py-2 mx-10">How <span className=" text-teal-600">reTrade</span> helps you 😉</h3>
+      </div>
+      <div className=" lg:flex gap-10 p-10 mx-auto justify-center items-center">
+      <div className='bg-gray-800 my-5 text-center shadow-lg p-5 rounded-xl hover:scale-110 transition duration-300 ease-in-out max-w-sm'>
+        <img className='mx-auto rounded-lg' src={service1} width={100} height={100}/>
+        <h3 className=' text-lg font-medium pt-8 pb-2'>Get Upskilled</h3>
+        <p className='text-md py-2 leading-8 text-white'>
+          The #1 way developers get experience on working wwith different developers
+        </p>
+      </div>
+
+      <div className='bg-gray-800 my-5 text-center shadow-lg p-5 rounded-xl hover:scale-110 transition duration-300 ease-in-out max-w-sm'>
+        <img className='mx-auto rounded-lg' src={service1} width={100} height={100}/>
+        <h3 className=' text-lg font-medium pt-8 pb-2'>Get Upskilled</h3>
+        <p className='text-md py-2 leading-8 text-white'>
+          The #1 way developers get experience on working wwith different developers
+        </p>
+      </div>
+
+      <div className='bg-gray-800 my-5 text-center shadow-lg p-5 rounded-xl hover:scale-110 transition duration-300 ease-in-out max-w-sm'>
+        <img className='mx-auto rounded-lg' src={service1} width={100} height={100}/>
+        <h3 className=' text-lg font-medium pt-8 pb-2'>Get Upskilled</h3>
+        <p className='text-md py-2 leading-8 text-white'>
+          The #1 way developers get experience on working wwith different developers
+        </p>
+      </div>
+      </div>
+    </section>
+
+    <section className=" text-black bg-white">
+      <div className=" p-2">
+        <h3 className=" text-2xl py-2 mx-10">How <span className=" text-teal-600">reTrade</span> helps you ! 😉</h3>
+      </div>
+      <div className=" flex gap-10 p-10 mx-auto lg:justify-center items-center">
+      <div className='bg-gray-200 my-5 text-center shadow-lg p-5 rounded-xl hover:scale-110 transition duration-300 ease-in-out max-w-sm'>
+        <img className='mx-auto rounded-lg' src={service1} width={100} height={100}/>
+        <h3 className=' text-lg font-medium pt-8 pb-2'>Get Upskilled</h3>
+        <p className='text-md py-2 leading-8 text-black'>
+          The #1 way developers get experience on working wwith different developers
+        </p>
+      </div>
+
+      <div className='bg-gray-200 my-5 text-center shadow-lg p-5 rounded-xl hover:scale-110 transition duration-300 ease-in-out max-w-sm'>
+        <img className='mx-auto rounded-lg' src={service1} width={100} height={100}/>
+        <h3 className=' text-lg font-medium pt-8 pb-2'>Get Upskilled</h3>
+        <p className='text-md py-2 leading-8 text-black'>
+          The #1 way developers get experience on working wwith different developers
+        </p>
+      </div>
+
+      <div className='bg-gray-200 my-5 text-center shadow-lg p-5 rounded-xl hover:scale-110 transition duration-300 ease-in-out max-w-sm'>
+        <img className='mx-auto rounded-lg' src={service1} width={100} height={100}/>
+        <h3 className=' text-lg font-medium pt-8 pb-2'>Get Upskilled</h3>
+        <p className='text-md py-2 leading-8 text-black'>
+          The #1 way developers get experience on working wwith different developers
+        </p>
+      </div>
+      </div>
+    </section>
+    
     <LandingPageFooter/>
     
   </div>
