3c3754c7adb1ec425a64369d5643c3490e44418f
Rahilsiddique <siddiquerahil19@gmail.com>
2023-02-02 15:54:41 +0530

    changes
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { FaSignInAlt } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { toast, ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
 import { login, reset } from "../features/auth/authSlice";
 import Spinner from "../components/Spinner";
 
@@ -31,7 +31,7 @@ const Login = () => {
         navigate("/feed");
       }
 
-      // dispatch(reset());
+      dispatch(reset());
     },
     [user, isError, isSuccess, message, navigate, dispatch]
   );
