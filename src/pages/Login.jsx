013a4ef337b3f8726316655d2948a41984f61f82
gweryf <priyam2007sp@gmail.com>
2023-02-02 12:45:09 +0530

    added basic form for signup and login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <form action="" className=" p-5 text-center my-5">
+      <div className=" flex gap-5 justify-center items-center py-3">
+        <label htmlFor="usern">Username</label>
+        <input type="text" id="usern" />
+      </div>
+      <div className=" flex gap-5 justify-center items-center py-3">
+        <label htmlFor="passd">Password</label>
+        <input type="text" id="passd" />
+      </div>
+      <div className=" mx-auto flex justify-center items-center gap-10 my-5">
+        <button className="btn" type="submit">Log-In</button>
+        <button className="btn">Create an Account</button>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
