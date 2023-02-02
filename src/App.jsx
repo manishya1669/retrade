75faa59c698c4231cf778551bf43c9ea1f7cb199
adityayadav71 <adityayadav1088@gmail.com>
2023-02-02 19:03:11 +0530

    added profile page and map service

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import {
-  BrowserRouter as Router,
-  Route, Routes
-} from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Feed from "./pages/Feed";
-import LandingPage from './pages/LandingPage';
+import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <>
       <Router>
-          <Navbar/>
-          <Routes>
-            <Route path="/feed" element={<Feed />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path='/' element={<LandingPage /> } />
-          </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
       </Router>
     </>
   );
