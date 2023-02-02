01faaf3bc1871a6083462c3936bee17a97d3687b
manishya1669 <manishya1669@gmail.com>
2023-02-02 18:41:46 +0530

    LoginPageUpdated

--- src/components/LandingPage/LandingPage_footer.jsx
+++ src/components/LandingPage/LandingPage_footer.jsx
@@ -4,12 +4,12 @@ import logo from "../Images/HackLogo.png"
 export default function Footer() {
   
     return (
-        <div  className="p-10 bg-gray-500 text-white">
+        <div  className=" bg-gray-500 text-white">
         <div className="flex h-100 w-100 p-10 gap-10 justify-around items-center">
           <div>
             <img  src={logo} width={300} height={100}  alt="logo"/>
           </div>
-          <div className=" p-5">
+          <div className=" ">
           <div className="text-5xl py-5 text-center">Exchange Safely and Securely !</div>
           <div className="text-2xl max-w-xl mx-auto text-center">reTrade takes great pride in offering a safe and secure online barting experience !!</div> 
           </div>
