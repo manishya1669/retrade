4040137948c135f827027255ebbc57bc32418ca9
Rahilsiddique <siddiquerahil19@gmail.com>
2023-02-02 13:46:15 +0530

    frontend auth
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { json, Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  //   const { user } = useSelector(state => state.auth);
+
+  const user = JSON.parse(localStorage.getItem("pocketbase_auth"));
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -10,14 +23,18 @@ const Navbar = () => {
         </a>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link to="/Login">Login</Link>
-          </li>
-          <li>
-            <Link to="/Register">Signup</Link>
-          </li>
-        </ul>
+        {user
+          ? <button className="btn" onClick={onLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          : <ul className="menu menu-horizontal px-1">
+              <li>
+                <Link to="/Login">Login</Link>
+              </li>
+              <li>
+                <Link to="/Register">Signup</Link>
+              </li>
+            </ul>}
       </div>
     </div>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,87 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { login, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 
 const Login = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  const { email, password } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    state => state.auth
+  );
+
+  useEffect(
+    () => {
+      if (isError) {
+        toast.error(message);
+      }
+
+      if (isSuccess || user) {
+        navigate("/feed");
+      }
+
+      dispatch(reset());
+    },
+    [user, isError, isSuccess, message, navigate, dispatch]
+  );
+
+  const onChange = e => {
+    setFormData(prevState => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const userData = { email, password };
+
+    dispatch(login(userData));
+  };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
   return (
-    <form action="" className=" p-5 text-center my-5">
+    <form className=" p-5 text-center my-5" onSubmit={onSubmit}>
       <div className=" flex gap-5 justify-center items-center py-3">
         <label htmlFor="usern">Username</label>
-        <input type="text" id="usern" />
+        <input
+          id="Email"
+          type="email"
+          placeholder="email"
+          value={email}
+          name="email"
+          className="input w-full max-w-xs border border-black"
+          onChange={onChange}
+        />
       </div>
       <div className=" flex gap-5 justify-center items-center py-3">
         <label htmlFor="passd">Password</label>
-        <input type="text" id="passd" />
+        <input
+          id="Password"
+          type="password"
+          placeholder="password"
+          value={password}
+          name="password"
+          className="input w-full max-w-xs border border-black"
+          onChange={onChange}
+        />
       </div>
       <div className=" mx-auto flex justify-center items-center gap-10 my-5">
-        <button className="btn" type="submit">Log-In</button>
+        <button className="btn" type="submit">
+          Log-In
+        </button>
         <button className="btn">Create an Account</button>
       </div>
     </form>
