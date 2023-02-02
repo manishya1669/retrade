dcd1f1f0e210fe3b2f13e28f33a7ef5753df2e07
Rahilsiddique <siddiquerahil19@gmail.com>
2023-02-02 12:09:29 +0530

    file structure
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import {
+  BrowserRouter as Router,
+  Route, Routes
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Feed from "./pages/Feed";
+import LandingPage from './pages/LandingPage';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
-  return <div className="text-2xl text-pink-400">lets go</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Navbar/>
+          <Routes>
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path='/' element={<LandingPage /> } />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">
+          <Link to="/">reTrade</Link>
+        </a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/Login">Login</Link>
+          </li>
+          <li>
+            <Link to="/Register">Signup</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
