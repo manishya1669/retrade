17c51639b901e767960b749664a73d23302dc099
gweryf <priyam2007sp@gmail.com>
2023-02-02 19:25:44 +0530

    added website layout

--- src/components/LandingPage/LandingPage_footer.jsx
+++ src/components/LandingPage/LandingPage_footer.jsx
@@ -4,14 +4,14 @@ import logo from "../Images/HackLogo.png"
 export default function Footer() {
   
     return (
-        <div  className="p-10 bg-gray-500 text-white">
+        <div  className=" bg-black text-white">
         <div className="flex h-100 w-100 p-10 gap-10 justify-around items-center">
           <div>
             <img  src={logo} width={200} height={100}  alt="logo"/>
           </div>
-          <div className=" p-5">
-          <div className="text-5xl py-5 text-center">Exchange Safely and Securely !</div>
-          <div className="text-2xl max-w-xl mx-auto text-center">reTrade takes great pride in offering a safe and secure online barting experience !!</div> 
+          <div className="">
+          <div className="text-4xl py-5 text-center">Exchange Safely and Securely</div>
+          <div className="text-md max-w-xl mx-auto text-center">reTrade takes great pride in offering a safe and secure online barting experience</div> 
           </div>
           </div>         
         </div>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -37,30 +37,30 @@ const LandingPage = () => {
 
     <section className=" text-white bg-black">
       <div className=" p-2">
-        <h3 className=" text-2xl py-2 mx-10">How <span className=" text-teal-600">reTrade</span> helps you 😉</h3>
+        <h3 className=" text-2xl py-2 mx-10">How <span className=" text-teal-600">reTrade</span> features helps you 😉</h3>
       </div>
       <div className=" lg:flex gap-10 p-10 mx-auto justify-center items-center">
       <div className='bg-gray-800 my-5 text-center shadow-lg p-5 rounded-xl hover:scale-110 transition duration-300 ease-in-out max-w-sm'>
         <img className='mx-auto rounded-lg' src={service1} width={100} height={100}/>
-        <h3 className=' text-lg font-medium pt-8 pb-2'>Get Upskilled</h3>
+        <h3 className=' text-lg font-medium pt-8 pb-2'>Bargain</h3>
         <p className='text-md py-2 leading-8 text-white'>
-          The #1 way developers get experience on working wwith different developers
+          Our website allow users to bargain on listed items to get the best value for the product and the wallet
         </p>
       </div>
 
       <div className='bg-gray-800 my-5 text-center shadow-lg p-5 rounded-xl hover:scale-110 transition duration-300 ease-in-out max-w-sm'>
-        <img className='mx-auto rounded-lg' src={service1} width={100} height={100}/>
-        <h3 className=' text-lg font-medium pt-8 pb-2'>Get Upskilled</h3>
+        <img className='mx-auto rounded-lg' src={service2} width={100} height={100}/>
+        <h3 className=' text-lg font-medium pt-8 pb-2'>Simple and Clean UI</h3>
         <p className='text-md py-2 leading-8 text-white'>
-          The #1 way developers get experience on working wwith different developers
+          We have a very simple and clean design making it very navigable irrespective of users experience with websites
         </p>
       </div>
 
       <div className='bg-gray-800 my-5 text-center shadow-lg p-5 rounded-xl hover:scale-110 transition duration-300 ease-in-out max-w-sm'>
-        <img className='mx-auto rounded-lg' src={service1} width={100} height={100}/>
-        <h3 className=' text-lg font-medium pt-8 pb-2'>Get Upskilled</h3>
+        <img className='mx-auto rounded-lg' src={service3} width={100} height={100}/>
+        <h3 className=' text-lg font-medium pt-8 pb-2'>Rating</h3>
         <p className='text-md py-2 leading-8 text-white'>
-          The #1 way developers get experience on working wwith different developers
+          Our website has a rating feature that allows users to rate dubious listings on the website and report the user
         </p>
       </div>
       </div>
@@ -68,30 +68,30 @@ const LandingPage = () => {
 
     <section className=" text-black bg-white">
       <div className=" p-2">
-        <h3 className=" text-2xl py-2 mx-10">How <span className=" text-teal-600">reTrade</span> helps you ! 😉</h3>
+        <h3 className=" text-2xl py-2 mx-10">Advantages of using <span className=" text-teal-600">reTrade</span> ! 😉</h3>
       </div>
       <div className=" flex gap-10 p-10 mx-auto lg:justify-center items-center">
       <div className='bg-gray-200 my-5 text-center shadow-lg p-5 rounded-xl hover:scale-110 transition duration-300 ease-in-out max-w-sm'>
-        <img className='mx-auto rounded-lg' src={service1} width={100} height={100}/>
-        <h3 className=' text-lg font-medium pt-8 pb-2'>Get Upskilled</h3>
+        <img className='mx-auto rounded-lg' src={service4} width={100} height={100}/>
+        <h3 className=' text-lg font-medium pt-8 pb-2'>Money</h3>
         <p className='text-md py-2 leading-8 text-black'>
-          The #1 way developers get experience on working wwith different developers
+          You can save a lot of money when u dont have to pay for stuff
+        </p>
+      </div>
+
+      <div className='bg-gray-200 my-5 text-center shadow-lg p-5 rounded-xl hover:scale-110 transition duration-300 ease-in-out max-w-sm'>
+        <img className='mx-auto rounded-lg' src={service5} width={100} height={100}/>
+        <h3 className=' text-lg font-medium pt-8 pb-2'>De-Clutering</h3>
+        <p className='text-md py-2 leading-8 text-black'>
+          Get rid of unwanted items by listing them here and get something useful in return
         </p>
       </div>
 
       <div className='bg-gray-200 my-5 text-center shadow-lg p-5 rounded-xl hover:scale-110 transition duration-300 ease-in-out max-w-sm'>
         <img className='mx-auto rounded-lg' src={service1} width={100} height={100}/>
-        <h3 className=' text-lg font-medium pt-8 pb-2'>Get Upskilled</h3>
+        <h3 className=' text-lg font-medium pt-8 pb-2'>No Payments</h3>
         <p className='text-md py-2 leading-8 text-black'>
-          The #1 way developers get experience on working wwith different developers
-        </p>
-      </div>
-
-      <div className='bg-gray-200 my-5 text-center shadow-lg p-5 rounded-xl hover:scale-110 transition duration-300 ease-in-out max-w-sm'>
-        <img className='mx-auto rounded-lg' src={service1} width={100} height={100}/>
-        <h3 className=' text-lg font-medium pt-8 pb-2'>Get Upskilled</h3>
-        <p className='text-md py-2 leading-8 text-black'>
-          The #1 way developers get experience on working wwith different developers
+        there won't be problems of balance of payments crisis since money is not invovled in exchanges
         </p>
       </div>
       </div>
